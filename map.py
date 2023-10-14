ec11f1970cbc683e2f360a828b31838feb90e8c7
Chumak Vladimir <parsival427@gmail.com>
2023-10-15 01:27:20 +0300

    second

--- map.py
+++ map.py
@@ -1,67 +1,10 @@
 import folium
 from folium.plugins import MarkerCluster
 from flask import Flask, render_template_string, request, jsonify
-import overpy
-from numpy import square
-import pandas as pd
-from sklearn.cluster import KMeans
-from sklearn.preprocessing import StandardScaler
+import modal
 
-
-cordinates = ["43.5739,39.6841,43.6138,39.7857","44.5251,33.3212,44.6823,33.7277","69.3246,87.9878,69.4024,88.3943","55.1100,61.2409,55.2360,61.6474","58.5104,31.1777,58.5680,31.3809","52.2504,104.191,52.3179,104.394"]
-cordinates_ekb = ["56.7139,60.3616,56.9555,61.1746",1112,1539371,22];
-cordinates_tula = ["54.0513,37.2121,54.3097,38.0251",145.8,548623,33];
-corsinates_cpb = ["59.4953,28.4656,60.3799,31.7175",1439,5600044,30];
-
-array = [["amenity","cafe",0],
-         ["highway","track",0],
-         ["amenity","clinic",1],
-         ["highway","footway",1],
-         ["footway","sidewalk",1],
-         ["cycleway","track",0],
-         ["cycleway","opposite_track",0],
-         ["amenity","traffic_park",0],
-         ["amenity","bicycle_parking",1],
-         ["amenity","bar",0],
-         ["amenity","fast_food",0],
-         ["amenity","food_court",0],
-         ["amenity","pub",0],
-         ["amenity","restaurant",0],
-         ["amenity","training",1],
-         ["amenity","dentist",1],
-         ["amenity","casino",0],
-         ["amenity","police",1],         
-         ["amenity","bicycle_repair_station",1],
-         ["amenity","marketplace",1],
-         ["boundary","national_park",1],
-         ["building","farm",1],
-         ["building","farm_auxiliary",1],
-         ["building","sports_hall",1],
-         ["building","stadium",1],
-         ["landuse","farmland",1],
-         ["landuse","farmyard",1],
-         ["landuse","winter_sports",1],
-         ["route","bicycle",1],
-         ["route","horse",1],
-         ["route","inline_skates",1],
-         ["route","tracks",0],
-         ["shop","farm",1],
-         ["shop","bicycle",1],
-         ["shop","sports",1],
-         ["shop","swimming_pool",1]]
-
-city_square = [176.8,864,23.16,530,460,280]
-population_array = [466078,340075,183299,1187960,1253030,61115]
-air_quality = [35,45,25,23,35,24]
-result_array = []
-count = []
-density_array = []
-density_positive = []
-density_negative = []
-
-api = overpy.Overpass()
 app = Flask(__name__)
-
+city_name = ""
 @app.route('/')
 def index():
     return render_template_string("""
@@ -116,7 +59,6 @@ def index():
                 #queryButton:hover {
                     background-color: #005f5f; /* Darker blue on hover */
                 }
-
             </style>
         </head>
         <body>
@@ -217,56 +159,21 @@ def index():
         </html>
     """)
 
+
 @app.route('/set_city', methods=['POST'])
 def set_city():
     city_name = request.form['cityName']
     print(f'Выбранный город: {city_name}')
-    test()
+    modal.city_name = city_name
+
     return "City set successfully", 200
 
 @app.route('/query_overpass', methods=['POST'])
 def query_overpass():
-    result2 = api.query("node[amenity=cafe]({{bbox}});out;")
-    print(result2)
-    # Обработка результатов запроса
-    return "Query was successful"  # возвращение ответа на клиент
+    modal.model_start()   
+    return "Query was successful"
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-def test():
-   print("есть")
-def click(rer):
-   for cordinate in cordinates:
-      new_array=[]
-      count_positive = 0
-      count_negative = 0
-      for item in array:
-        result = api.query(f"node[{item[0]}={item[1]}]({cordinate});out;")
-        len_nodes = len(result.nodes)
-        new_array.append(len_nodes)
-      if(item[2]==0):
-         count_negative+=len_nodes
-      elif(item[2]==1):
-         count_positive+=len_nodes
-      count.append([count_positive, count_negative])
-      result_array.append(new_array)
-      
-   df = pd.DataFrame(count, columns=["positive","negativ"], index=["Сочи","Севастополь","Норильск","Челябинск","Нижний Новгород","Иркутск"])
-   for i in range(len(city_square)):
-      density_array.append(population_array[i]/city_square[i])
-   df = df.assign(density=density_array)
-   for i in range(len(count)):
-      density_negative.append(count[i][0]/city_square[i])
-      density_positive.append(count[i][1]/city_square[i])
-   df = df.assign(density_negative=density_negative)
-   df = df.assign(density_positive=density_positive)
-   df = df.assign(air_quality=air_quality)
-   features = ['density','positive','negativ','density_positive','density_negative','air_quality']
-   X = df[features]
-   scaler = StandardScaler()
-   X_scaled = scaler.fit_transform(X)
-   num_clusters = 3
-   kmeans = KMeans(n_clusters=num_clusters, random_state=42)
-   df['Cluster'] = kmeans.fit_predict(X_scaled)
-   print(df)
+
---
+++ modal.py
@@ -0,0 +1,97 @@
+import overpy
+from numpy import square
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+
+tags = [["amenity","cafe",0],
+         ["highway","track",0],
+         ["amenity","clinic",1],
+         ["highway","footway",1],
+         ["footway","sidewalk",1],
+         ["cycleway","track",0],
+         ["cycleway","opposite_track",0],
+         ["amenity","traffic_park",0],
+         ["amenity","bicycle_parking",1],
+         ["amenity","bar",0],
+         ["amenity","fast_food",0],
+         ["amenity","food_court",0],
+         ["amenity","pub",0],
+         ["amenity","restaurant",0],
+         ["amenity","training",1],
+         ["amenity","dentist",1],
+         ["amenity","casino",0],
+         ["amenity","police",1],         
+         ["amenity","bicycle_repair_station",1],
+         ["amenity","marketplace",1],
+         ["boundary","national_park",1],
+         ["building","farm",1],
+         ["building","farm_auxiliary",1],
+         ["building","sports_hall",1],
+         ["building","stadium",1],
+         ["landuse","farmland",1],
+         ["landuse","farmyard",1],
+         ["landuse","winter_sports",1],
+         ["route","bicycle",1],
+         ["route","horse",1],
+         ["route","inline_skates",1],
+         ["route","tracks",0],
+         ["shop","farm",1],
+         ["shop","bicycle",1],
+         ["shop","sports",1],
+         ["shop","swimming_pool",1]]
+
+date_ekb = ["56.7139,60.3616,56.9555,61.1746",1112,1539371,22,"Екатеринбург"];
+date_tula = ["54.0513,37.2121,54.3097,38.0251",145.8,548623,33,"Тула"];
+date_cpb = ["59.4953,28.4656,60.3799,31.7175",1439,5600044,30,"Санкт-Петербург"];
+city_name = ""
+
+def model_start():
+    csv_file_path = "data.csv"
+    df_base = pd.read_csv(csv_file_path, index_col="Unnamed: 0")
+    city = date_cpb
+    if(city_name=="Екатеринбург"):
+      city = date_ekb
+    elif(city_name=="Тула"):
+      city = date_tula
+    elif(city_name=="Санкт-Петербург"):
+      city = date_cpb
+
+    data_city = collecting_date_city(city)
+    df_child = pd.DataFrame([data_city],index=[f'{city[4]}'])
+    df = df_base._append(df_child)
+    print(df)
+
+    features = ['density','positive','negativ','density_positive','density_negative','air_quality']
+
+    X = df[features]
+
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+
+    kmeans = KMeans(n_clusters=3)
+    df['Cluster'] = kmeans.fit_predict(X_scaled)
+    feature_importance = kmeans.cluster_centers_.mean(axis=0)
+
+    importance_df = pd.DataFrame({'Feature': df.columns[:-1], 'Importance': feature_importance})
+    importance_df = importance_df.sort_values(by='Importance', ascending=False)
+    print(importance_df.head())
+
+def collecting_date_city(city):
+  api = overpy.Overpass()
+  
+  count_positive = 0
+  count_negative = 0
+  for item in tags:
+    result = api.query(f"node[{item[0]}={item[1]}]({city[0]});out;")
+    len_nodes = len(result.nodes)
+    if(item[2]==0):
+      count_negative+=len_nodes
+    elif(item[2]==1):
+      count_positive+=len_nodes 
+  density = city[2]/city[1]
+  density_negative = count_negative/city[1]
+  density_positive = count_positive/city[1]
+  
+  data_city = {'positive':count_positive,'negativ':count_negative,'density':density,'density_negative':density_negative,'density_positive':density_positive,'air_quality':city[3]}
+  return data_city
